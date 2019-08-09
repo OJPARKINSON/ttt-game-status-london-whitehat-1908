7e0eb000b983e0666c88e9ec56844f715e54115c
Oliver Parkinson <olliepar01@gmail.com>
2019-08-09 13:43:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,43 +1,29 @@
 # Helper Method
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
 def full?(board) 
   x = 0
-  count = 0
+  wcount = 0
   while x >= 8 do
     if board[x] == "X" || board[x] == "O" then
-      count = count + 1
+      wcount = wcount + 1
     end
-    if board[x] == " " then
+    elsif board[x] == " " then
       return false
     end
   end
-end
-
-def won?(board)
-  WIN_COMBINATIONS = [
-    [0,1,2], 
-    [3,4,5],
-    [6,7,8],
-    [0,4,8],
-    [2,4,6],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8]
-  ]
-  for board in WIN_COMBINATIONS do
-    win_index_1 = WIN_COMBINATIONS[0]
-    win_index_2 = WIN_COMBINATIONS[1]
-    win_index_3 = WIN_COMBINATIONS[2]
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
-  else
-    false
-  end
-end
 end
 
 def draw(board) 
